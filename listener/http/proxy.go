45e54d0f80595283dc297ee45d04d8094c901eab
zfh <zfh@abc.com>
2024-04-19 10:26:35 +0800

    debug

--- listener/http/proxy.go
+++ listener/http/proxy.go
@@ -45,10 +45,6 @@ func HandleConn(c net.Conn, in chan<- C.ConnContext, authenticator auth.Authenti
 
 		if !trusted {
 			err, resp = authenticate(ctx, request, authenticator)
-			if err != nil {
-				fmt.Fprintf(c, "error: %s", err.Error())
-				break
-			}
 			request = request.WithContext(ctx.GetContext())
 			trusted = resp == nil
 		}
@@ -103,6 +99,7 @@ func HandleConn(c net.Conn, in chan<- C.ConnContext, authenticator auth.Authenti
 
 		err = resp.Write(conn)
 		if err != nil {
+			fmt.Fprintf(c, "error: %s", err.Error())
 			break // close connection
 		}
 	}
